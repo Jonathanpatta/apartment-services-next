992015e522803d4d11d3d845511be2365025017c
Jonathanpatta <=>
2023-03-05 10:26:32 +0530

    items view

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Headers", value: "*" },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
